4a8b7e64cd6cb661f351fa9ef9be9eb9a6306a67
BarnabyUCN <10407536@ucn.dk>
2022-11-12 20:29:42 +0100

    sql connection trying
    i need aform

---
+++ back-end/API/Database/frmMain.cs
@@ -0,0 +1,32 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.CompilerServices;
+
+namespace ConnectPostgres
+{
+        public partial class frmMain : Form
+        { 
+
+        private string connstring = String.Format("Server={0};Port={1};" +
+            "User Id={2};Password={3};Database={4};",
+            "localhost", 5432, "postgres", "Adamisthebest", "Chat Mayhem");
+        private NpgsqlConnection conn;
+
+        public frmMain()
+        {
+            initializeComponent();
+        }
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            conn = new NpgsqlConnection(connstring);
+        }
+    }
+}

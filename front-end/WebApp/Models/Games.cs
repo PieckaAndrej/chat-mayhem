a5b28a849eb869c108d36a07bbb1bd2a3f5268dd
CosminRaita <93519047+CosminRaita@users.noreply.github.com>
2022-11-16 15:07:51 +0100

    Create game pt2

---
+++ front-end/WebApp/Models/Games.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApp.Models
+{
+    public class Games
+    {
+        public List<Game> games { get; set; }
+
+        public Games()
+        {
+            games = new List<Game>();
+            games.Add(new Game(15, "1", 1, 1));
+            games.Add(new Game(15, "1", 1, 1));
+            games.Add(new Game(15, "1", 1, 1));
+            games.Add(new Game(15, "1", 1, 1));
+            games.Add(new Game(15, "1", 1, 1));
+        }
+    }
+}

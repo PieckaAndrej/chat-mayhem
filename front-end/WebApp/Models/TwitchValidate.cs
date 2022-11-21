95ed714b660b0aa558b146700e37a003a8ab348d
PieckaAndrej <10407548@ucn.dk>
2022-11-21 19:28:13 +0100

    twitch validate

---
+++ front-end/WebApp/Models/TwitchValidate.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp.Models
+{
+    public class TwitchValidate
+    {
+        public string? clientId { get; set; }
+        public string? login { get; set; }
+        public List<string>? scopes { get; set; }
+        public string? userId { get; set; }
+        public int expiresIn { get; set; }
+    }
+}
--- front-end/WebApp/Services/TwitchService.cs
+++ front-end/WebApp/Services/TwitchService.cs
@@ -1,6 +1,22 @@
-﻿namespace WebApp.Services
+﻿using RestSharp;
+using RestSharp.Authenticators.OAuth2;
+using System.Security.Policy;
+using WebApp.Models;
+
+namespace WebApp.Services
 {
     public class TwitchService
     {
+        public TwitchValidate? ValidateToken(string token)
+        {
+            RestClient restClient = new RestClient("https://id.twitch.tv/");
+
+            RestRequest restRequest = new RestRequest("oauth2/validate");
+            restClient.Authenticator = new OAuth2AuthorizationRequestHeaderAuthenticator(token);
+
+            var response = restClient.ExecutePost<TwitchValidate>(restRequest);
+
+            return response.Data;
+        }
     }
 }

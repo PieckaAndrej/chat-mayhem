f16ab02f7231dc871f54dbe5abf744ce3dbd4998
PieckaAndrej <10407548@ucn.dk>
2022-11-21 14:12:30 +0100

    Twitch authorization
    Aziz plz fix the injection in the twitch index

---
+++ front-end/WebApp/BusinessLogic/GameLogic.cs
@@ -0,0 +1,23 @@
+﻿using RestSharp;
+using WebApp.Models;
+using WebApp.Services;
+
+namespace WebApp.BusinessLogic
+{
+    public class GameLogic
+    {
+        private readonly GameService _gameService;
+
+        public GameLogic()
+        {
+            _gameService = new GameService();
+        }
+
+        public Game? CreateGame(Game game)
+        {
+            RestResponse<Game> result = _gameService.CreateGame(game).Result;
+
+            return result.Data;
+        }
+    }
+}
---
+++ front-end/WebApp/Services/GameService.cs
@@ -0,0 +1,21 @@
+﻿using RestSharp;
+using WebApp.Models;
+
+namespace WebApp.Services
+{
+    public class GameService
+    {
+        private readonly RestClient _client;
+
+        public GameService()
+        {
+            _client = new RestClient("https://localhost:7200/");
+        }
+
+        public async Task<RestResponse<Game>> CreateGame(Game game)
+        {
+            var request = new RestRequest("api/Game").AddJsonBody(game);
+            return await _client.ExecutePostAsync<Game>(request);
+        }
+    }
+}

5ebce02cc6ebc6f00af8072aba09999cee714356
PieckaAndrej <10407548@ucn.dk>
2022-11-21 20:51:59 +0100

    RestSharp deserialization doesn't work

--- front-end/WebApp/Services/TwitchService.cs
+++ front-end/WebApp/Services/TwitchService.cs
@@ -10,11 +10,31 @@ namespace WebApp.Services
         public TwitchValidate? ValidateToken(string token)
         {
             RestClient restClient = new RestClient("https://id.twitch.tv/");
-
-            RestRequest restRequest = new RestRequest("oauth2/validate");
             restClient.Authenticator = new OAuth2AuthorizationRequestHeaderAuthenticator(token);
 
-            var response = restClient.ExecutePost<TwitchValidate>(restRequest);
+            RestRequest restRequest = new RestRequest("oauth2/validate");
+
+            var response = restClient.ExecuteGet<TwitchValidate>(restRequest);
+
+            Console.WriteLine(response.ResponseStatus);
+
+            return response.Data;
+        }
+
+        public TwitchToken? GetTwitchToken(string code)
+        {
+            RestClient restClient = new RestClient("https://id.twitch.tv/");
+
+            RestRequest restRequest = new RestRequest("oauth2/token");
+            restRequest.AddParameter("code", code);
+            restRequest.AddParameter("grant_type", "authorization_code");
+            restRequest.AddParameter("client_id", "8hmbxjfogmmj9e14y2ohn2vb0q8zv5");
+            restRequest.AddParameter("client_secret", "");
+            restRequest.AddParameter("redirect_uri", "https://localhost:7026/twitch");
+
+            var response = restClient.ExecutePost<TwitchToken>(restRequest);
+
+            Console.WriteLine(response.ResponseStatus);
 
             return response.Data;
         }

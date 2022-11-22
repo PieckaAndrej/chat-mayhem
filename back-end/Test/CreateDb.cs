2711d189a9db11c0d83fc452637966716da07d7f
AzizKadem <93520332+AzizKadem@users.noreply.github.com>
2022-11-22 15:24:01 +0100

    Update CreateDb.cs

--- back-end/Test/CreateDb.cs
+++ back-end/Test/CreateDb.cs
@@ -6,10 +6,30 @@ namespace Test
 {
     public class CreateDb
     {
-        IConfiguration configuration = new ConfigurationBuilder()
+        private static IConfiguration configuration = new ConfigurationBuilder()
                 .AddJsonFile($"appsettings.json", optional: false)
                 .Build();
         private CreateDb()
+        {
+            
+
+        }
+
+        private static CreateDb instance = null;
+        public static CreateDb Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new CreateDb();
+                    create();
+                }
+                return instance;
+            }
+        }
+
+        private static void create()
         {
             var s = "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity " +
                "WHERE pg_stat_activity.datname = 'Chat Mayhem Test' AND pid <> pg_backend_pid(); " +
@@ -22,20 +42,6 @@ namespace Test
             {
                 connection.Open();
                 connection.Execute(s);
-            }
-
-        }
-
-        private static CreateDb instance = null;
-        public static CreateDb Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = new CreateDb();
-                }
-                return instance;
             }
         }
 

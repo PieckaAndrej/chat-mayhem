0fdb376f2c4db42717851a50bdb7dc7e30701329
CosminRaita <93519047+CosminRaita@users.noreply.github.com>
2022-11-23 15:16:01 +0100

    Chat frontend

---
+++ front-end/WebApp/Services/ChatService.cs
@@ -0,0 +1,23 @@
+﻿using RestSharp;
+using WebApp.Models;
+
+namespace WebApp.Services
+{
+    public class ChatService
+    {
+        private readonly RestClient _client;
+
+        public ChatService()
+        {
+            _client = new RestClient("https://localhost:7200/");
+        }
+
+        public async Task<string> CheckViewerAnswer(string answer)
+        {
+            var request = new RestRequest("api/Chat").AddJsonBody(answer);
+            var response = await _client.ExecutePostAsync<string>(request);
+
+            return response.Content;
+        }
+    }
+}

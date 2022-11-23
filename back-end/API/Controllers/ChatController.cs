106bf429c7758d7f7bba7c7f7a016a289aeb6b2d
CosminRaita <93519047+CosminRaita@users.noreply.github.com>
2022-11-23 14:27:12 +0100

    Submit answer endpoint

---
+++ back-end/API/Controllers/ChatController.cs
@@ -0,0 +1,44 @@
+﻿using API.DTOs;
+using API.Services;
+using Data.DatabaseLayer;
+using Data.ModelLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController
+    {
+        private ChatService chatService;
+
+        public ChatController(IConfiguration inConfiguration)
+        {
+            ServiceInjector.Con = inConfiguration.GetConnectionString("ChatMayhem Connection") ?? "No connection string";
+
+            string? connectionString = inConfiguration.GetConnectionString("ChatMayhem Connection");
+            if (connectionString == null)
+            {
+                Console.WriteLine("Connection string is null");
+            }
+
+            chatService = new ChatService(new ChatAccess(connectionString ?? "No connection string"));
+        }
+
+        [HttpGet]
+        public ActionResult<string> GetAnswer(string answer)
+        {
+            string viewerAnswer = ServiceInjector.chatService.GetAnswer(answer);
+
+            if (viewerAnswer.Equals("Invalid answer"))
+            {
+                return "Wrong";
+            }
+            else
+            {
+                return viewerAnswer;
+            }
+        }
+
+    }
+}

4b317801611f020ea65deb8f4bc7ba01e7bd1d5a
CosminRaita <93519047+CosminRaita@users.noreply.github.com>
2022-11-23 15:16:01 +0100

    Chat frontend

--- front-end/WebApp/Controllers/ChatController.cs
+++ front-end/WebApp/Controllers/ChatController.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Services;
 
 namespace WebApp.Controllers
 {
     public class ChatController : Controller
     {
-        public IActionResult Index()
+        private ChatService? chatService;
+
+        /*public async Task<IActionResult> Index(string answer)
         {
+            string viewerAnswer = chatService?.CheckViewerAnswer(answer);
+
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+
             return View();
-        }
+        }*/
     }
 }

15a2f396d32cf43cbb0833e0437bc3d2984e1018
CosminRaita <93519047+CosminRaita@users.noreply.github.com>
2022-11-25 10:48:40 +0100

    Signalr changes

--- front-end/WebApp/Hubs/ChatHub.cs
+++ front-end/WebApp/Hubs/ChatHub.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using WebApp.Services;
 
 namespace WebApp.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        private ChatService? chatService;
+
+        public async Task SendMessage(string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            message = (await chatService.CheckViewerAnswer(message)).Content;
+           await Clients.All.SendAsync("ReceiveMessage", message);
         }
     }
 }

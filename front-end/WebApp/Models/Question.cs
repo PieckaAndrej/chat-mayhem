c9123f2965df65635af56575fce2a708b7e773b5
CosminRaita <93519047+CosminRaita@users.noreply.github.com>
2022-11-29 14:06:37 +0100

    Switched question methods to service

--- front-end/WebApp/Models/Question.cs
+++ front-end/WebApp/Models/Question.cs
@@ -11,49 +11,10 @@ namespace WebApp.Models
         public string prompt { get; set; }
         public List<ViewerAnswer> viewerAnswers { get; set; }
 
-        public static string con = new ConfigurationBuilder()
-                                .AddJsonFile("appsettings.json", optional: false)
-                                .Build().GetSection("ConnectionString").Value;
-
-        public Question()
+        public Question(string prompt, List<ViewerAnswer> viewerAnswers)
         {
-
-        }
-
-        public bool InsertAnswers(List<Question> questions)
-        {
-            bool inserted = true;
-
-            var client = new MongoClient(con);
-
-            var db = client.GetDatabase("ChatMayhem");
-
-            var coll = db.GetCollection<Question>("Viewer'sAnswers");
-
-            if(questions == null)
-            {
-                inserted = false; 
-            }
-
-            coll.InsertMany(questions);
-
-            return inserted;
-        }
-        
-
-        public List<Question> GetAnswers(string questionPrompt)
-        {
-            List<Question> answers = new List<Question>();
-
-            var client = new MongoClient(con);
-
-            var db = client.GetDatabase("ChatMayhem");
-
-            var coll = db.GetCollection<Question>("Viewer'sAnswers");
-
-            answers = coll.Find(x => x.prompt == questionPrompt).ToList();
-
-            return answers;
+            this.prompt = prompt;
+            this.viewerAnswers = viewerAnswers;
         }
     }
 }
---
+++ front-end/WebApp/Services/QuestionService.cs
@@ -0,0 +1,47 @@
+﻿using MongoDB.Driver;
+using WebApp.Models;
+
+namespace WebApp.Services
+{
+    public class QuestionService
+    {
+        public static string con = new ConfigurationBuilder()
+                                .AddJsonFile("appsettings.json", optional: false)
+                                .Build().GetSection("ConnectionString").Value;
+
+        public bool InsertAnswers(List<Question> questions, string collection)
+        {
+            bool inserted = true;
+
+            var client = new MongoClient(con);
+
+            var db = client.GetDatabase("ChatMayhem");
+
+            var coll = db.GetCollection<Question>(collection);
+
+            if (questions == null)
+            {
+                inserted = false;
+            }
+
+            coll.InsertMany(questions);
+
+            return inserted;
+        }
+
+        public List<Question> GetAnswers(string questionPrompt, string collection)
+        {
+            List<Question> answers = new List<Question>();
+
+            var client = new MongoClient(con);
+
+            var db = client.GetDatabase("ChatMayhem");
+
+            var coll = db.GetCollection<Question>(collection);
+
+            answers = coll.Find(x => x.prompt == questionPrompt).ToList();
+
+            return answers;
+        }
+    }
+}

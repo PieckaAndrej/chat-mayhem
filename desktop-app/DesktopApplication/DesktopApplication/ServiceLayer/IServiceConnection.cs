b9b0f31110c73f009e7f8c548021ac650ae97c45
CosminRaita <93519047+CosminRaita@users.noreply.github.com>
2022-12-01 14:08:33 +0100

    Service Setup

---
+++ desktop-app/DesktopApplication/DesktopApplication/ServiceLayer/IServiceConnection.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonServiceClientDesktop.Servicelayer
+{
+    public interface IServiceConnection
+    {
+        public string? BaseUrl{ get; init;}
+        public string? UseUrl { get; set; }
+        Task<HttpResponseMessage?> Get();
+        Task<HttpResponseMessage?> Post(StringContent postJson);
+        Task<HttpResponseMessage?> Put(StringContent putJson);
+        Task<HttpResponseMessage?> Delete();
+    }
+}

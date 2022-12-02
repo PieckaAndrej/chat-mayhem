98a756fd05ddd3023d7b6cf3cac001594e18f39b
BarnabyUCN <10407536@ucn.dk>
2022-12-02 10:47:34 +0100

    moved question access

--- back-end/DatabaseAccess/DatabaseLayer/QuestionAccess.cs
+++ back-end/DatabaseAccess/DatabaseLayer/QuestionAccess.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Dapper;
+using Data.ModelLayer;
+using Data.DatabaseLayer;
+using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,30 @@ using System.Threading.Tasks;
 
 namespace Data.DatabaseLayer
 {
-    public interface QuestionAccess
+    internal class QuestionAccess : IQuestionAccess
     {
+        private readonly string _connectionString;
+
+        public QuestionAccess(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public Question InsertQuestion(Question question)
+        {
+            string sql = "INSERT INTO public.\"Question\"" +
+                "(\"questionPackId\", \"text\") " +
+                "VALUES (@questionPackId, @text) RETURNING id;";
+
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                question.id = connection.QuerySingle<int>(sql, new
+                {
+                    questionPackId = question.QuestionPack.Id,
+                    text = question.text
+                });
+
+                return question;
+            }
+        }
     }
 }
